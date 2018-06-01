a40aeeaf0040fd01da3dfe46873e4e5982da347e
Kevin <kevin@cerda.io>
2018-05-31 21:05:40 -0700

    Database connected, server is listening.

---
+++ db/config/index.js
@@ -0,0 +1,18 @@
+const Sequelize = require('sequelize');
+
+const db = new Sequelize('adb', '', '', {
+  host: 'localhost',
+  dialect: 'postgres',
+});
+
+db.authenticate()
+  .then(() => {
+    console.log('Successfully connected to database');
+  })
+  .catch(err => {
+    console.log('Unable to connect to the database');
+  });
+
+  module.exports = {
+    db
+  }
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const parser = require('body-parser');
 const path = require('path');
+const db = require('../db/config/')
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.use(parser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '/static')));
 
 const PORT = 3000;
+
 app.listen(PORT, (err) => {
   err ? console.log('Error connecting to server: ', err) : console.log('Successfully connected to server on port', PORT);
 });
