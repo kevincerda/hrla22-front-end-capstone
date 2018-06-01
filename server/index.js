775a6c158d75c56129ef5bf4fb17aba9df67dbc9
Kevin <kevin@cerda.io>
2018-05-31 20:36:59 -0700

    Server is listening.

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const parser = require('body-parser');
+const path = require('path');
+
+const app = express();
+
+app.use(parser.json());
+app.use(parser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '/static')));
+
+const PORT = 3000;
+app.listen(PORT, (err) => {
+  err ? console.log('Error connecting to server: ', err) : console.log('Successfully connected to server on port', PORT);
+});
