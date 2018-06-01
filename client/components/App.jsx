ae7a726d17f489645b720e170996fe55a6a38808
Kevin <kevin@cerda.io>
2018-05-31 22:26:39 -0700

    Client skeleton.

---
+++ client/components/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export default class Home extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        Hello, Mars.
+      </div>
+    )
+  }
+}
