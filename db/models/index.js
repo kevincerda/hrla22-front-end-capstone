482e8328552346aa6987269918e4eb3ac934c638
Kevin <kevin@cerda.io>
2018-06-02 12:09:19 -0700

    ProductList and Review models defined.

--- db/models/index.js
+++ db/models/index.js
@@ -1,2 +1,21 @@
 const Sequelize = require('sequelize');
 const db = require('../config');
+
+const ProductList = Sequelize.define('product', {
+  id: Sequelize.INTEGER,
+  name: Sequelize.STRING
+});
+
+const Review = Sequelize.define('review', {
+  id: Sequelize.INTEGER,
+  customer_id: Sequelize.INTEGER,
+  rating: Sequelize.INTEGER,
+  title: Sequelize.STRING,
+  date: Sequelize.STRING,
+  review: Sequelize.STRING,
+  helpful_count: Sequelize.INTEGER,
+  product_id: Sequelize.INTEGER,
+});
+
+module.exports.ProductList = ProductList;
+module.exports.Review = Review;
