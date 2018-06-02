850179c0510cf25e4f637368622663554408a377
Kevin <kevin@cerda.io>
2018-06-02 12:08:19 -0700

    Component structure defined. Rendering hardcorded data to begin styling.

--- client/index.jsx
+++ client/index.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from './components/App.jsx'
+import App from './components/App.jsx';
+import WebFont from 'webfontloader';
+
+WebFont.load({
+  google: {
+    families: ['Lato:400,700', 'sans-serif']
+  }
+});
 
 render(<App />, document.getElementById('app'));
