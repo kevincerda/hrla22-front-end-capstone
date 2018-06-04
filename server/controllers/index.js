83f91d0dc1b55c12ab5fc896f4cf684ded2aca52
Kevin <kevin@cerda.io>
2018-06-03 17:10:10 -0700

    Controller skeleton.

---
+++ server/controllers/index.js
@@ -0,0 +1,15 @@
+const Sequelize = require('sequelize');
+const { Product, Review } = require('../../db/models');
+
+const ReviewCtrl = {
+  'GET': {
+
+  },
+  'POST': {
+
+  }
+};
+
+module.exports = {
+  ReviewCtrl,
+}
