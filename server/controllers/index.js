8177a71f63939663f5ffb3265ff4c19b13f60e72
Kevin <kevin@cerda.io>
2018-06-03 22:53:41 -0700

    Review controller fetching data from db based on ID search.

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,12 +1,16 @@
 const Sequelize = require('sequelize');
+const Promise = require('bluebird');
 const { Product, Review } = require('../../db/models');
 
 const ReviewCtrl = {
-  'GET': {
-
-  },
-  'POST': {
-
+  'GET': (req, res) => {
+    let query = {};
+    if (typeof JSON.parse(req.params.search) === 'number') {
+      query.product_id = req.params.search;
+    }
+    Review.findAll({ where: query })
+      .then(data => res.status(200).send(data))
+      .catch(err => res.status(404).send(err));
   }
 };
 
